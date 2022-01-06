97eb7f7f32500bae1784082d7546529d79ca9a32
Anita Cheung <acheung@grailbio.com>
2022-01-05 19:32:57 -0800

    first commit

---
+++ wilson_score.py
@@ -0,0 +1,23 @@
+"""
+Reference: https://www.mikulskibartosz.name/wilson-score-in-python-example/
+"""
+
+class WilsonScore:
+    def __init__(self):
+        """"""
+    
+    def wilson(p, n, z=1.96):
+        """ Gets the Wilson Score.
+
+        Keyword arguments:
+        p -- sample proportion (e.g passing)
+        n -- sample size
+        z -- expected confidence interval of Wilson score
+        
+        returns:
+        """
+    
+    def information(self):
+        """Describes the purpose of the Wilson Score"""
+        description = "Estimates population probability; Proportion of an outcome for a binomial distribution (two possibilities, e.g. pass or fail)"
+        print(description)
